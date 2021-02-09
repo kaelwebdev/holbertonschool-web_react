9b929df8f8ca3f729e93ee5dfba9b48bb9f40237
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-09 17:19:36 -0500

    adding other tags to the table

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -24,9 +24,25 @@ const studentsList:Array<Student> = [
     student2
 ]
 
-const table:HTMLTableElement = document.createElement('table')
+const table:HTMLTableElement = document.createElement('table');
+const thead: HTMLTableSectionElement = document.createElement('thead');
+const tbody: HTMLTableSectionElement = document.createElement('tbody');
+const tr_thead: HTMLTableRowElement = document.createElement('tr');
+const td1_tr_thead: HTMLTableCellElement = document.createElement('td');
+const td2_tr_thead: HTMLTableCellElement = document.createElement('td');
+
+td1_tr_thead.innerHTML = 'firstName';
+td2_tr_thead.innerHTML = 'location';
+
+table.append(thead);
+table.append(tbody);
+thead.append(tr_thead);
+tr_thead.append(td1_tr_thead);
+tr_thead.append(td2_tr_thead);
+
 studentsList.forEach((student) => {
-    const row:HTMLTableRowElement = table.insertRow()
+    const row:HTMLTableRowElement = table.insertRow();
+    tbody.append(row);
     for (const [k, v] of Object.entries(student)) {
         if (k === 'firstName' || k === 'location') {
             const cell:HTMLTableCellElement = row.insertCell();
