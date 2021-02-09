dd4dd200a30c484a079cbf144acd73bb1f5d8de8
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-09 17:42:25 -0500

    [Fix] lint

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -19,36 +19,36 @@ const student2: Student = {
     location: 'Francia'
 }
 
-const studentsList:Array<Student> = [
+const studentsList: Array<Student> = [
     student1,
     student2
 ]
 
-const table:HTMLTableElement = document.createElement('table');
+const table: HTMLTableElement = document.createElement('table');
 const thead: HTMLTableSectionElement = document.createElement('thead');
 const tbody: HTMLTableSectionElement = document.createElement('tbody');
-const tr_thead: HTMLTableRowElement = document.createElement('tr');
-const th1_tr_thead: HTMLTableCellElement = document.createElement('th');
-const th2_tr_thead: HTMLTableCellElement = document.createElement('th');
+const trHead: HTMLTableRowElement = document.createElement('tr');
+const th1: HTMLTableCellElement = document.createElement('th');
+const th2: HTMLTableCellElement = document.createElement('th');
 
-th1_tr_thead.innerHTML = 'firstName';
-th2_tr_thead.innerHTML = 'location';
+th1.innerHTML = 'firstName';
+th2.innerHTML = 'location';
 
 table.append(thead);
 table.append(tbody);
-thead.append(tr_thead);
-tr_thead.append(th1_tr_thead);
-tr_thead.append(th2_tr_thead);
+thead.append(trHead);
+trHead.append(th1);
+trHead.append(th2);
 
 studentsList.forEach((student) => {
-    const row:HTMLTableRowElement = table.insertRow();
+    const row: HTMLTableRowElement = table.insertRow();
     tbody.append(row);
-    const cell:HTMLTableCellElement = row.insertCell();
-    const text:Text = document.createTextNode(student.firstName);
+    const cell: HTMLTableCellElement = row.insertCell();
+    const text: Text = document.createTextNode(student.firstName);
     cell.appendChild(text);
 
-    const cell2:HTMLTableCellElement = row.insertCell();
-    const text2:Text = document.createTextNode(student.location);
+    const cell2: HTMLTableCellElement = row.insertCell();
+    const text2: Text = document.createTextNode(student.location);
     cell2.appendChild(text2);
 })
 document.body.appendChild(table)
