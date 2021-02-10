e65d7bfb280a876d1d7ae9a49ab0d02c004ce85e
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-09 22:32:22 -0500

    [Update] implementing isDirector function and executeWork function

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -44,3 +44,17 @@ export function createEmployee(salary: number | string): Director | Teacher {
   if (typeof salary === 'number' && salary < 500) return new Teacher();
   return new Director();
 }
+
+export function isDirector(employee: DirectorInterface | TeacherInterface): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+}
+
+export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+  if (isDirector(employee)) {
+    console.log(employee.workDirectorTasks());
+    return employee.workDirectorTasks();
+  }
+  
+  console.log(employee.workTeacherTasks());
+  return employee.workTeacherTasks();
+}
