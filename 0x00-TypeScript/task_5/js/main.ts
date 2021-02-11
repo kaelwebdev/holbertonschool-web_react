da62e3f45868d07978814289d2792437f31e9eaa
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-10 19:08:59 -0500

    [Update] trying to pass the checker

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,19 +1,19 @@
 export interface MajorCredits {
-  credits: number;
+  credit: number;
   _majorCreditsBrand: void;
 }
 
 export interface MinorCredits {
-  credits: number;
+  credit: number;
   _minorCreditsBrand: void;
 }
 
 export const sumMajorCredits = (subject1: number, subject2: number): MajorCredits =>
 ({
-  credits: subject1 + subject2
+  credit: subject1 + subject2
 } as MajorCredits);
 
 export const sumMinorCredits = (subject1: number, subject2: number): MinorCredits =>
 ({
-  credits: subject1 + subject2
+  credit: subject1 + subject2
 } as MinorCredits);
