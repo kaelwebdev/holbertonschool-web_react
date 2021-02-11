5151d28760e1a3b6ef6e863bf93317f3f0f08587
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-10 19:57:30 -0500

    [fix] trying to pass the checker

--- 0x00-TypeScript/task_5/js/main.ts
+++ 0x00-TypeScript/task_5/js/main.ts
@@ -1,4 +1,4 @@
-type UUID = string
+type UUID = number
 
 export interface MajorCredits {
   credits: number;
@@ -10,10 +10,16 @@ export interface MinorCredits {
   brand: UUID;
 }
 
+let idCount = 0;
+let idCount2 = 0;
+
+
 export function sumMajorCredits (subject1: MajorCredits, subject2: MajorCredits): MajorCredits {
-  return { credits: subject1.credits + subject2.credits } as MajorCredits
+  idCount = idCount + 1;
+  return { credits: subject1.credits + subject2.credits, brand: idCount } as MajorCredits
 }
 
 export function sumMinorCredits (subject1: MinorCredits, subject2: MinorCredits): MinorCredits {
-  return { credits: subject1.credits + subject2.credits } as MinorCredits
+  idCount2 = idCount2 + 1;
+  return { credits: subject1.credits + subject2.credits, brand: idCount2  } as MinorCredits
 }
