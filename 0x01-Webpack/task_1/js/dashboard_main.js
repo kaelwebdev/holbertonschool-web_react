1f34b121b34d7086d863b161d0fa678eee95e46c
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-12 13:05:46 -0500

    [Create] example using webpack config

---
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append(
+  '<p>Holberton Dashboard</p>',
+  '<p>Dashboard data for the students</p>',
+  '<button><p>Click here to get started<p></button>',
+  '<p id="count"></p>',
+  '<p>Copyright - Holberton School</p>'
+);
+
+let count = 0;
+
+const updateCounter = () => {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 1000));
