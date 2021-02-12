1f1cde5255e8344e7ad70647a4e74345773a21a9
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-12 13:29:24 -0500

    [Fix] try to pass the checker

--- 0x01-Webpack/task_1/js/dashboard_main.js
+++ 0x01-Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,20 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append('<p id="count"></p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+const body = document.getElementsByTagName("body")[0];
+const p1 = document.createElement('p');
+const p2 = document.createElement('p');
+const button = document.createElement('button');
+const p3 = document.createElement('p');
+const p4 = document.createElement('p');
+
+p1.innerHTML = "Holberton Dashboard";
+p2.innerHTML = "Dashboard data for the students";
+button.innerHTML = "Click here to get started";
+p3.setAttribute('id', 'count');
+p4.innerHTML = "Copyright - Holberton School";
+
+body.append(p1, p2, button, p3, p4)
 
 let count = 0;
 
