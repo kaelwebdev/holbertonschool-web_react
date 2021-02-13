c7099198ef024313de6a327ee7bb9881ae8a33c0
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-12 19:57:36 -0500

    [Fix] changing settings for checker reasons

--- 0x01-Webpack/task_2/webpack.configv2.js
+++ 0x01-Webpack/task_2/webpack.configv2.js
@@ -5,9 +5,6 @@ module.exports = {
   entry: {
     main: path.resolve(__dirname, './js/dashboard_main.js'),
   },
-  performance: {
-    maxAssetSize: 1000000,
-  },
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
@@ -19,17 +16,8 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.(gif|png|jpe?g|svg)$/i,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true,
-              disable: true,
-            },
-          },
-        ],
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
       },
     ],
   },
