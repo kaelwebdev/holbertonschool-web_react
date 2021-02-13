102ddcfb26327febca0d86b6112ac27834489a1b
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-12 19:18:11 -0500

    [Create] the necessary webpack configuration for images and styles was implemented

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,28 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+import '../css/main.css';
+import mainImage from '../assets/holberton-logo.jpg';
+
+const img = document.createElement("div");
+// img.src = mainImage;
+img.style.background = mainImage;
+// img.style.backgroundImage = "url('../assets/holberton-logo.jpg')";
+img.id = 'logo';
+
+$('body').append(img);
+$('#imagen').css("background-image", "url(../assets/holberton-logo.jpg)"); 
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
+
+const updateCounter = () => {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 100));
---
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  experiments: {
+    asset: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource'
+      },
+    ],
+  },
+};
