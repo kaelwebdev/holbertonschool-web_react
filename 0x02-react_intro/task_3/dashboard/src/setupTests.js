768bead601b5c34ebe6cf0b88433f3f3c6f85d05
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-16 18:43:29 -0500

    trying to pass the checker

--- 0x02-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x02-react_intro/task_3/dashboard/src/setupTests.js
@@ -4,5 +4,5 @@
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom';
 import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
