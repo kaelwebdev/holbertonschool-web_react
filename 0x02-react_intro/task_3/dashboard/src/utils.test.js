f5f2eafdad620ab9f03c30229c35024d70218d64
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-17 02:43:12 -0500

    [Update] a group description was added

--- 0x02-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x02-react_intro/task_3/dashboard/src/utils.test.js
@@ -2,15 +2,17 @@
 const { getFullYear, getFooterCopy, getLatestNotification } = require('./utils');
 const currentYear = new Date().getFullYear() + '';
 
-it('Year returns current year', () => {
-  expect(getFullYear()).toBe(currentYear);
-})
+describe('Test - utils.test.js', () => {
+  it('Year returns current year', () => {
+    expect(getFullYear()).toBe(currentYear);
+  });
 
-it('getFooterCopy works in both cases', () => {
-  expect(getFooterCopy(true)).toBe("Holberton School");
-  expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
-})
+  it('getFooterCopy works in both cases', () => {
+    expect(getFooterCopy(true)).toBe("Holberton School");
+    expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
+  });
 
-it('getLatestNotification works', () => {
-  expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
-})
+  it('getLatestNotification works', () => {
+    expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
+  });
+});
