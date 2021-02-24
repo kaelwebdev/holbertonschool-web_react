cc1769598858f133ac7a51b87242073d3074491a
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-23 22:27:54 -0500

    new tests and PropTypes added

--- 0x03-react_props/task_2/dashboard/src/App/App.test.js
+++ 0x03-react_props/task_2/dashboard/src/App/App.test.js
@@ -17,4 +17,13 @@ describe("App.test.js", () => {
   it('renders App-footer', () => {
     expect(wrapper.find('div.App-footer').exists()).toEqual(true);
   });
+  it('renders Header', () => {
+    expect(wrapper.find('Header').exists()).toEqual(true);
+  });
+  it('renders Login', () => {
+    expect(wrapper.find('Login').exists()).toEqual(true);
+  });
+  it('renders Footer', () => {
+    expect(wrapper.find('Footer').exists()).toEqual(true);
+  });
 });
