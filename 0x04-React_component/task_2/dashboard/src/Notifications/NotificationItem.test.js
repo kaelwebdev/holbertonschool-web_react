b824925697ffcb76dabf17aebff7cebf17d2c9ab
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-27 23:55:19 -0500

    tests were implemented for notifications

--- 0x04-React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
+++ 0x04-React_component/task_2/dashboard/src/Notifications/NotificationItem.test.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import NotificationItem from './NotificationItem';
-import { shallow } from 'enzyme';
-
-// const wrapper = shallow(<NotificationItem/>);
+import { shallow, mount  } from 'enzyme';
+import Notifications from './Notifications'
 
 describe("NotificationItem.test.js", () => {
-  it('correct component rendering', () => {
-    shallow(<NotificationItem />);
+  let wrapper;
+
+  afterEach(() => {
+    wrapper.unmount();
   });
+
+  it('correct component rendering', () => {
+    wrapper = shallow(<NotificationItem />);
+  });
+
   it('correct component rendering - case1 - value, type', () => {
     const type = "default";
     const value = "test";
     const html = undefined;
-    const wrapper = shallow(<NotificationItem type={ type } value={ value }/>);
+    wrapper = shallow(<NotificationItem type={ type } value={ value }/>);
     const li = wrapper.find('li');
     expect(li.exists()).toEqual(true);
     expect(li.props()['data-notification-type']).toEqual("default");
@@ -22,9 +28,34 @@ describe("NotificationItem.test.js", () => {
 
   it('correct component rendering - case2 - html', () => {
     const html = '<u>test</u>';
-    const wrapper = shallow(<NotificationItem html={{ __html: '<u>test</u>' }}/>);
+    wrapper = shallow(<NotificationItem html={{ __html: '<u>test</u>' }}/>);
     const li = wrapper.find('li');
     expect(li.exists()).toEqual(true);
     expect(wrapper.html()).toEqual(`<li data-notification-type=\"default\">${ html }</li>`);
   });
+});
+
+describe("NotificationItem.test.js - events", () => {
+  let wrapper;
+
+  afterEach(() => {
+    wrapper.unmount();
+  });
+
+  it('function markAsRead - correct log message -case 1', () => {
+
+    const ns = [
+      {id: 1, html: undefined, type: "default", value: "a"},
+      {id: 2, html: undefined, type: "default", value: "b"}
+    ];
+
+    const consoleSpy = jest.spyOn(console, 'log');
+    wrapper = mount(<Notifications displayDrawer={ true } listNotifications={ ns }/>);
+    wrapper.find(NotificationItem).at(0).simulate('click');
+    expect(consoleSpy).toHaveBeenCalledWith('Notification 1 has been marked as read');
+    wrapper.find(NotificationItem).at(1).simulate('click');
+    expect(consoleSpy).toHaveBeenCalledWith('Notification 2 has been marked as read');
+    consoleSpy.mockRestore();
+  });
+
 });
