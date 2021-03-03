ce50961db8f5a032bdb8b2c9ad183ad8f411f1ed
kaelwebdev <cdcp-1795@hotmail.com>
2021-03-03 18:39:07 -0500

    replicating task_2

---
+++ 0x05-React_inline_styling/task_3/dashboard/src/Header/Header.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from '../assets/holberton-logo.jpg';
+import {StyleSheet, css} from 'aphrodite';
+
+
+export default class Header extends Component {
+  render() {
+    return (
+      <>
+        <img src={ logo } className={css(styles.img)} alt="logo" />
+        <h1 className={css(styles.h1)}>School dashboard</h1>
+      </>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  img: {
+    width: '14rem'
+  },
+  h1: {
+    fontWeight: 'bold'
+  },
+});
