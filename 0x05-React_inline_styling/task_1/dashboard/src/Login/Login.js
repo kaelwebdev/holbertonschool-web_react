4ffa19097a7af2344327f0dc71323f7065f326ea
kaelwebdev <cdcp-1795@hotmail.com>
2021-03-03 12:58:25 -0500

    implementing styles with aphrodite

--- 0x05-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x05-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import './Login.css';
-
+import {StyleSheet, css} from 'aphrodite';
 
 export default class Login extends Component {
 
@@ -8,14 +7,32 @@ export default class Login extends Component {
     return (
       <>
         <p>Login to access the full dashboard</p>
-        <form className="loginForm">
+        <form className={ css(styles.loginForm) }>
           <label htmlFor="email">Email: </label>
-          <input id="email" type="text" />
+          <input id="email" type="text" className={css(styles.inputForm)} />
           <label htmlFor="password"> Password: </label>
-          <input id="password" type="text" />
-          <button id="btnForm">OK</button>
+          <input id="password" type="text" className={css(styles.inputForm)} />
+          <button id="btnForm" className={css(styles.btnForm)}>OK</button>
         </form>
       </>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  loginForm: {
+    padding: '20px 0px',
+  },
+  inputForm: {
+    border: '1px solid #dfdfdf',
+    borderRadius: '10px',
+    marginRight: '10px',
+  },
+  btnForm: {
+    padding: '3px 6px',
+    background: 'white',
+    border: '1px solid #dfdfdf',
+    borderRadius: '3px',
+    cursor: 'pointer',
+  }
+});
