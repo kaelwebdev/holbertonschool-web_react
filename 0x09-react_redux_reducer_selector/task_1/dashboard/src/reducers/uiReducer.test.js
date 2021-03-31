18915611ecb0192aeffe8f6fdcb9c12c21eead03
kaelwebdev <cdcp-1795@hotmail.com>
2021-03-31 16:15:17 -0500

    cp task_0

---
+++ 0x09-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -0,0 +1,27 @@
+import { uiReducer } from './uiReducer';
+
+describe('Test reducer.js', () => {
+  const initialState = {
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {},
+  };
+
+  it('initial state when no action is passed', (done) => {
+    const r = uiReducer(initialState, '');
+    expect(initialState).toEqual(r);
+    done();
+  });
+
+  it('action SELECT_COURSE action works as expected', (done) => {
+    const r = uiReducer(initialState, 'SELECT_COURSE');
+    expect(initialState).toEqual(r);
+    done();
+  });
+
+  it('action DISPLAY_NOTIFICATION_DRAWER action works as expected', (done) => {
+    const r = uiReducer(initialState, { type: 'DISPLAY_NOTIFICATION_DRAWER' });
+    expect(r.isNotificationDrawerVisible).toEqual(true);
+    done();
+  });
+});
