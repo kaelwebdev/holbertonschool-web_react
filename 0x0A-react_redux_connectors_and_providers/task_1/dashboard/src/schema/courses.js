4ed308a4e23577f03fffd68c26579a607fc0c694
kaelwebdev <cdcp-1795@hotmail.com>
2021-04-06 21:24:29 -0500

    deleting files

--- 0x0A-react_redux_connectors_and_providers/task_1/dashboard/src/schema/courses.js
+++
@@ -1,6 +0,0 @@
-import { normalize, schema } from 'normalizr';
-
-const courses = new schema.Entity('courses');
-export const coursesNormalizer = (data) => {
-  return normalize(data, [courses]).entities.courses;
-};
